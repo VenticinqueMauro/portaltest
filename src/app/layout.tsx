958d93855d1a554e2fb276be18a7adb9c481bf33
Venticinque Mauro <mauro.venticinque@hotmail.com>
2024-03-15 09:07:25 -0300

    config middleware and test without clerk auth

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,7 @@
 
 import type { Metadata } from "next";
-import { ClerkProvider } from '@clerk/nextjs'
 import { Inter } from "next/font/google";
 import "./globals.css";
-import Nav from "@/components/Nav";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -18,13 +16,10 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <ClerkProvider>
-      <html lang="en">
-        <body className={`${inter.className} flex justify-center items-center p-24`}>
-          <Nav />
-          {children}
-        </body>
-      </html>
-    </ClerkProvider>
+    <html lang="en">
+      <body className={`${inter.className} flex justify-center items-center p-24`}>
+        {children}
+      </body>
+    </html>
   );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,48 @@
+'use client';
 
 export default function page() {
-  
+
+  async function handleLogin() {
+    const res = await fetch('/api/auth/login', {
+      method: 'POST',
+      credentials: 'include',
+      body: JSON.stringify({
+        "email": "test@test.com",
+        "password": "123456",
+      }),
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    })
+    const data = await res.json()
+    console.log(data)
+  }
+
+  async function handleLogout() {
+    const res = await fetch('/api/auth/logout', {
+      method: "DELETE",
+      credentials: 'include'
+    })
+    const data = await res.json()
+    console.log(data)
+  }
+
+  async function handleProfile() {
+    const res = await fetch('/api/auth/profile', {
+      method: "GET",
+      credentials: 'include'
+    })
+    const data = await res.json()
+    console.log(data)
+  }
+
   return (
     <main>
       Next Auth
+      <a href="/dashboard" className="block mt-5">Dashboard</a>
+      <button onClick={handleLogin} className="block px-3 py-2 rounded bg-emerald-500 mt-5">Login</button>
+      <button onClick={handleLogout} className="block px-3 py-2 rounded bg-red-500 mt-5">Logout</button>
+      <button onClick={handleProfile} className="block px-3 py-2 rounded bg-blue-500 mt-5">Profile</button>
     </main>
   )
 }
