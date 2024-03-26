72508d311cf4219a6c1c5eab654d09cc67b7867d
Venticinque Mauro <mauro.venticinque@hotmail.com>
2024-03-26 14:32:03 -0300

    add creacion provisoria de modelo news

---
+++ src/models/news.ts
@@ -0,0 +1,67 @@
+import { Schema, model, models, Document } from "mongoose";
+
+enum NewsStatus {
+    INACTIVE = 'inactive',
+    ACTIVE = 'active',
+    PENDING = 'pending',
+}
+
+interface NewsDocument extends Document {
+    title: string;
+    content: string;
+    author: string;
+    category?: string[]; 
+    tags?: string[];
+    image: string;
+    status: NewsStatus;
+    comments?: string[]; 
+    createdAt: Date;
+    updatedAt: Date;
+}
+
+const NewsSchema = new Schema<NewsDocument>({
+    title: {
+        type: String,
+        required: true,
+        minlength: 3
+    },
+    content: {
+        type: String,
+        required: true,
+        minlength: 3
+    },
+    author: {
+        type: String,
+        required: true,
+        minlength: 3
+    },
+    category: {
+        type: [String], 
+    },
+    tags: {
+        type: [String], 
+    },
+    image: {
+        type: String,
+        required: true,
+    },
+    status: {
+        type: String,
+        enum: Object.values(NewsStatus),
+        default: NewsStatus.INACTIVE
+    },
+    comments: {
+        type: [String], 
+        default: []
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now
+    },
+    updatedAt: {
+        type: Date,
+        default: Date.now
+    }
+});
+
+export const News = models.New || model<NewsDocument>('News', NewsSchema);
