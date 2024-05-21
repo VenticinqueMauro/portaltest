c4c7c3996015d7c513d592517a287ff937d03d70
Venticinque Mauro <mauro.venticinque@hotmail.com>
2024-05-21 11:29:57 -0300

    add agregado de cotizaciones de portal cana en slider home

--- src/components/cotizaciones/Quotes.container.tsx
+++ src/components/cotizaciones/Quotes.container.tsx
@@ -1,8 +1,16 @@
 import {
     Carousel,
     CarouselContent,
-    CarouselItem
+    CarouselItem,
+    CarouselNext,
+    CarouselPrevious,
 } from "@/components/ui/carousel";
+import {
+    Accordion,
+    AccordionContent,
+    AccordionItem,
+    AccordionTrigger,
+} from "@/components/ui/accordion"
 
 const urls = [
     "https://dolarapi.com/v1/dolares/oficial",
@@ -36,6 +44,69 @@ const formatName = (quote: QuoteApi) => {
     return formatNameMap[key] || quote.nombre;
 };
 
+const formatPortalData = (data: any) => {
+    // Filtrar las propiedades _id y __v antes de procesar los datos
+    const filteredData = Object.entries(data).reduce((acc: any, [key, value]) => {
+        if (key !== '_id' && key !== '__v') {
+            acc[key] = value;
+        }
+        return acc;
+    }, {});
+
+    // Transformar los datos filtrados
+    return Object.entries(filteredData).map(([key, value]) => {
+        let title;
+        if (key === 'azucarTucuman') title = 'Azúcar Tucumán';
+        else if (key === 'azucarInternacional') title = 'Azúcar Internacional';
+        else if (key === 'biocombustible') title = 'Biocombustibles';
+        else title = key;
+
+        const variants = Object.entries(value as any).map(([variantKey, variantValue]: [string, any]) => {
+            let name, precioActual;
+            if (key === 'azucarTucuman') {
+                if (variantKey === '50kg') {
+                    name = 'Bolsa 50kg';
+                    precioActual = `$${variantValue.precioActual} + IVA`;
+                } else if (variantKey === '1kg') {
+                    name = 'Fracc/1kg';
+                    precioActual = `$${variantValue.precioActual} + IVA`;
+                } else {
+                    name = variantKey;
+                    precioActual = `$${variantValue.precioActual}`;
+                }
+            } else if (key === 'azucarInternacional') {
+                if (variantKey === 'londresN5') {
+                    name = 'Londres N5';
+                    precioActual = `US$${variantValue.precioActual}`;
+                } else if (variantKey === 'eeuuN11') {
+                    name = 'EEUU N11';
+                    precioActual = `US$${variantValue.precioActual}`;
+                } else {
+                    name = variantKey;
+                    precioActual = `US$${variantValue.precioActual}`;
+                }
+            } else if (key === 'biocombustible') {
+                if (variantKey === 'bioetanol de caña') {
+                    name = 'Bioetanol de caña';
+                    precioActual = `($/L) ${variantValue.precioActual}`;
+                } else if (variantKey === 'bioetanol de maiz') {
+                    name = 'Bioetanol de maíz';
+                    precioActual = `($/L) ${variantValue.precioActual}`;
+                } else {
+                    name = variantKey;
+                    precioActual = variantValue.precioActual;
+                }
+            } else {
+                name = variantKey;
+                precioActual = variantValue.precioActual;
+            }
+            return { name, precioActual };
+        });
+
+        return { title, variants };
+    });
+};
+
 type Currency = 'USD' | 'EUR' | 'BRL';
 type QuoteApi = {
     moneda: Currency,
@@ -55,11 +126,12 @@ async function getQuotations(): Promise<QuoteApi[]> {
         return [];
     }
 }
+
 async function getPortalQuotations(): Promise<QuoteApi[]> {
     try {
-        const responses = await fetch(`${process.env.NEXT_PUBLIC_URL}api/quotations`, { next: { revalidate: 3600 } }).then(response => response.json());
+        const responses = await fetch(`${process.env.NEXT_PUBLIC_URL}api/quotations`, { next: { revalidate: 1 } }).then(response => response.json());
 
-        return responses;
+        return responses.data;
     } catch (error) {
         console.error('Error al hacer las consultas:', error);
         return [];
@@ -72,18 +144,40 @@ export default async function QuotesContainer() {
     const quotations = await getQuotations();
     const portal = await getPortalQuotations();
 
+    const formattedPortalData = formatPortalData(portal[0]);
+
     return (
-        <div className="bg-black px-3 py-3 lg:px-0">
+        <div className="bg-black px-3 py-3">
             <Carousel className="max-w-7xl mx-auto">
-                <CarouselContent className="-ml-1">
+                <CarouselContent className="-ml-1 gap-10">
                     {quotations?.map((quote: QuoteApi) => (
-                        <CarouselItem key={quote.nombre + quote.venta} className="pl-2 md:pl-1 basis-auto lg:basis-1/5">
-                            <p className="text-tdn bg-white px-3 py-1 rounded w-fit text-sm md:text-base">
+                        <CarouselItem key={quote.nombre + quote.venta} className={`pl-2 md:pl-1 basis-auto `}>
+                            <p className="text-tdn bg-white px-3 py-1 rounded w-[200px] text-sm md:text-base flex justify-center gap-1">
                                 <b>{formatName(quote)}:</b> {formatQuote(quote)}
                             </p>
                         </CarouselItem>
                     ))}
+                    {formattedPortalData.map((item) => (
+                        <CarouselItem key={item.title} className={` pl-2 md:pl-1 basis-auto `}>
+                            <Accordion type="single" collapsible >
+                                <AccordionItem value={item.title} className="border-none min-w-[200px]">
+                                    <AccordionTrigger className="text-tdn bg-white px-3 py-1 rounded w-fit text-sm md:text-base space-x-3">
+                                        <b>{item.title}</b>
+                                    </AccordionTrigger>
+                                    <AccordionContent className="text-tdn bg-white px-3 py-1 text-sm md:text-base border-none mt-1">
+                                        {item.variants.map((variant) => (
+                                            <p key={variant.name} >
+                                                <b className="font-medium">{variant.name}</b>: {variant.precioActual}
+                                            </p>
+                                        ))}
+                                    </AccordionContent>
+                                </AccordionItem>
+                            </Accordion>
+                        </CarouselItem>
+                    ))}
                 </CarouselContent>
+                <CarouselPrevious className="text-white border-none bg-black  hidden lg:flex" />
+                <CarouselNext className="text-white border-none bg-black  hidden lg:flex" />
             </Carousel>
         </div>
     )
