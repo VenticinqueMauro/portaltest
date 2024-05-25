91669ff77e6d20bd1dd2028f01b48f37a9d7aade
Venticinque Mauro <mauro.venticinque@hotmail.com>
2024-05-25 08:31:47 -0300

    add botones para cambiar noticias de los slider a modo test estetico

--- src/components/home/MasLeidas.tsx
+++ src/components/home/MasLeidas.tsx
@@ -1,7 +1,9 @@
 import {
     Carousel,
     CarouselContent,
-    CarouselItem
+    CarouselItem,
+    CarouselNext,
+    CarouselPrevious
 } from "@/components/ui/carousel";
 import Image from "next/image";
 import SectionTitle from "./SectionTitle";
@@ -45,52 +47,56 @@ export default async function MasLeidas() {
         return null;
     }
 
-    console.log('MAS LEIDAS',mostReads)
+    console.log('MAS LEIDAS', mostReads)
 
     return (
         <div className="max-w-6xl lg:mr-[240px] 2xl:mx-auto lg:py-20 px-3">
             <SectionTitle title='Las más leídas' />
-            <Carousel className="px-3 ">
-                <CarouselContent className="-ml-1 flex gap-4">
-                    {mostReads?.map((item: MostRead) => (
-                        <CarouselItem key={item._id} className="pl-2 md:pl-1 lg:basis-1/5 md:hover:shadow md:hover:bg-gray-50 transition-all duration-100 cursor-pointer max-w-[400px]">
-                            <Link href={`${item.category}/${item.path}`} className="rounded flex flex-col justify-start gap-1">
-                                <div className="px-1">
-                                    {(
-                                        item.media.portada.type !== 'video' ? (
-                                            <Image
-                                                src={item.media.portada.url}
-                                                alt={item.title}
-                                                width={400}
-                                                height={300}
-                                                className="rounded aspect-video object-cover"
-                                                placeholder="blur"
-                                                blurDataURL={blurImage}
-                                            />
-                                        ) : (
-                                            <video
-                                                width="400"
-                                                height="300"
-                                                controls={true}
-                                                autoPlay
-                                                loop
-                                                className="w-full object-cover aspect-video rounded"
-                                            >
-                                                <source src={item.media.portada.url} type="video/mp4" />
-                                                Tu navegador no soporta la etiqueta de video.
-                                            </video>
-                                        )
-                                    )}
-                                </div>
-                                <div className="text-start px-1">
-                                    <p className="text-sm font-bold text-muted-foreground">{item.pretitle}</p>
-                                    <p className="font-semibold tracking-tight line-clamp-3">{item.title}</p>
-                                </div>
-                            </Link>
-                        </CarouselItem>
-                    ))}
-                </CarouselContent>
-            </Carousel>
+            <div className="px-12">
+                <Carousel className="px-3 max-w-5xl mx-auto">
+                    <CarouselContent className="-ml-1 flex gap-4">
+                        {mostReads?.map((item: MostRead) => (
+                            <CarouselItem key={item._id} className="pl-2 md:pl-1 lg:basis-1/5 md:hover:shadow md:hover:bg-gray-50 transition-all duration-100 cursor-pointer max-w-[400px]">
+                                <Link href={`${item.category}/${item.path}`} className="rounded flex flex-col justify-start gap-1">
+                                    <div className="px-1">
+                                        {(
+                                            item.media.portada.type !== 'video' ? (
+                                                <Image
+                                                    src={item.media.portada.url}
+                                                    alt={item.title}
+                                                    width={400}
+                                                    height={300}
+                                                    className="rounded aspect-video object-cover"
+                                                    placeholder="blur"
+                                                    blurDataURL={blurImage}
+                                                />
+                                            ) : (
+                                                <video
+                                                    width="400"
+                                                    height="300"
+                                                    controls={true}
+                                                    autoPlay
+                                                    loop
+                                                    className="w-full object-cover aspect-video rounded"
+                                                >
+                                                    <source src={item.media.portada.url} type="video/mp4" />
+                                                    Tu navegador no soporta la etiqueta de video.
+                                                </video>
+                                            )
+                                        )}
+                                    </div>
+                                    <div className="text-start px-1">
+                                        <p className="text-sm font-bold text-muted-foreground">{item.pretitle}</p>
+                                        <p className="font-semibold tracking-tight line-clamp-3">{item.title}</p>
+                                    </div>
+                                </Link>
+                            </CarouselItem>
+                        ))}
+                    </CarouselContent>
+                    <CarouselPrevious className="" />
+                    <CarouselNext className="" />
+                </Carousel>
+            </div>
         </div>
     )
 }
--- src/components/home/categories/MoreNewsSlider.tsx
+++ src/components/home/categories/MoreNewsSlider.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import SectionTitle from '../SectionTitle';
-import { Carousel, CarouselContent, CarouselItem } from '@/components/ui/carousel';
+import { Carousel, CarouselContent, CarouselItem, CarouselNext, CarouselPrevious } from '@/components/ui/carousel';
 import Image from 'next/image';
 import { blurImage } from '@/utils/blurImage';
 import { MoreNews, MoreNewsData } from '@/app/api/news/more-news/route';
@@ -36,45 +36,54 @@ const MoreNewsSlider: React.FC<Props> = ({ title, moreNews, category, sectionDat
     return (
         <div className="max-w-6xl 2xl:mx-auto px-3 lg:mr-[240px]">
             <SectionTitle title={`Más de ${title}`} />
-            <Carousel className="px-3">
-                <CarouselContent className="-ml-1 flex gap-4 ">
-                    {filteredNews.map((item: MoreNews) => (
-                        <CarouselItem key={item._id} className="pl-2 md:pl-1 lg:basis-1/5 md:hover:shadow md:hover:bg-gray-50 transition-all duration-100 cursor-pointer max-w-[400px]">
-                            <Link href={`${item.category}/${item.path}`} className="rounded flex flex-col justify-start gap-1">
-                                <div className="px-1">
-                                    {item.media.portada.type !== 'video' ? (
-                                        <Image
-                                            src={item.media.portada.url}
-                                            alt={item.title}
-                                            width={400}
-                                            height={300}
-                                            className="rounded aspect-video object-cover"
-                                            placeholder="blur"
-                                            blurDataURL={blurImage}
-                                        />
-                                    ) : (
-                                        <video
-                                            width="400"
-                                            height="300"
-                                            controls={true}
-                                            autoPlay
-                                            loop
-                                            className="w-full object-cover aspect-video rounded"
-                                        >
-                                            <source src={item.media.portada.url} type="video/mp4" />
-                                            Tu navegador no soporta la etiqueta de video.
-                                        </video>
-                                    )}
-                                </div>
-                                <div className="text-start px-1">
-                                    <p className="text-sm font-bold text-muted-foreground">{item.pretitle}</p>
-                                    <p className="font-semibold tracking-tight line-clamp-3">{item.title}</p>
-                                </div>
-                            </Link>
-                        </CarouselItem>
-                    ))}
-                </CarouselContent>
-            </Carousel>
+            <div className="px-12">
+                <Carousel className="px-3 max-w-5xl mx-auto">
+                    <CarouselContent className="-ml-1 flex gap-4 ">
+                        {filteredNews.map((item: MoreNews) => (
+                            <CarouselItem key={item._id} className="pl-2 md:pl-1 lg:basis-1/5 md:hover:shadow md:hover:bg-gray-50 transition-all duration-100 cursor-pointer max-w-[400px]">
+                                <Link href={`${item.category}/${item.path}`} className="rounded flex flex-col justify-start gap-1">
+                                    <div className="px-1">
+                                        {item.media.portada.type !== 'video' ? (
+                                            <Image
+                                                src={item.media.portada.url}
+                                                alt={item.title}
+                                                width={400}
+                                                height={300}
+                                                className="rounded aspect-video object-cover"
+                                                placeholder="blur"
+                                                blurDataURL={blurImage}
+                                            />
+                                        ) : (
+                                            <video
+                                                width="400"
+                                                height="300"
+                                                controls={true}
+                                                autoPlay
+                                                loop
+                                                className="w-full object-cover aspect-video rounded"
+                                            >
+                                                <source src={item.media.portada.url} type="video/mp4" />
+                                                Tu navegador no soporta la etiqueta de video.
+                                            </video>
+                                        )}
+                                    </div>
+                                    <div className="text-start px-1">
+                                        <p className="text-sm font-bold text-muted-foreground">{item.pretitle}</p>
+                                        <p className="font-semibold tracking-tight line-clamp-3">{item.title}</p>
+                                    </div>
+                                </Link>
+                            </CarouselItem>
+                        ))}
+                    </CarouselContent>
+                    {
+                        filteredNews.length > 1 &&
+                        <>
+                            <CarouselPrevious className="" />
+                            <CarouselNext className="" />
+                        </>
+                    }
+                </Carousel>
+            </div>
         </div>
     );
 }
