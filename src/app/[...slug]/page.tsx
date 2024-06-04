14bf014f89244cf8294806573743e27d42edecfa
Venticinque Mauro <102001296+VenticinqueMauro@users.noreply.github.com>
2024-06-04 14:26:09 -0300

    test [...slug]

--- src/app/[...slug]/page.tsx
+++ src/app/[...slug]/page.tsx
@@ -1,48 +1,42 @@
 import QuotesContainer from "@/components/cotizaciones/Quotes.container";
 import { getCover } from "@/components/home/Container.home";
-import SectionTitle from "@/components/home/SectionTitle";
-import NoticiaPrincipalCategory from "@/components/home/categories/NoticiaPrincipal.category";
-import NoticiasGridCategory from "@/components/home/categories/NoticiasGrid.category";
-import InferiorDesktop from "@/components/home/publicidades/Inferior.Desktop";
 import Navbar from "@/components/navbar/Navbar";
+import PortadaPrincipal from "@/components/noticias-por-categoria/PortadaPrincipal";
 import PublicidadSuperior from "@/components/noticias-por-categoria/publicidades/PublicidadSuperior";
 import Container from "@/components/noticiasIndividuales/Container";
 import { MainNews, NewsType, SidebarItem } from "@/types/news.types";
-import { blurImage } from "@/utils/blurImage";
 import { getFormatedCategoryNews, getNewsByPath } from "@/utils/utils";
-import Image from "next/image";
-import Link from "next/link";
 import { notFound } from "next/navigation";
-import { fontMerriweather } from "../fonts";
 
 export async function generateStaticParams() {
-    const response = await fetch(`${process.env.NEXT_PUBLIC_URL}api/news`, { cache: 'no-store' });
-    const { data }: { data: NewsType[] } = await response.json();
+    try {
+        const response = await fetch(`${process.env.NEXT_PUBLIC_URL}api/news`, { cache: 'no-store' });
+        if (!response.ok) throw new Error('Failed to fetch news data');
+        const { data }: { data: NewsType[] } = await response.json();
 
-    return data.map((news: NewsType) => {
-        const path = news.title ? news.title.replace(/\s+/g, '-').toLowerCase() : 'default-path';
-        return {
-            slug: [news.category, path]
-        };
-    });
+        return data.map((news: NewsType) => {
+            const path = news.title ? news.title.replace(/\s+/g, '-').toLowerCase() : 'default-path';
+            return {
+                slug: [news.category, path]
+            };
+        });
+    } catch (error) {
+        console.error('Error in generateStaticParams:', error);
+        return [];
+    }
 }
 
 async function getAds(category: string) {
-
     const encodedCategory = encodeURIComponent(category);
 
     try {
         const response = await fetch(`${process.env.NEXT_PUBLIC_URL}api/ads?category=${encodedCategory}`, { next: { revalidate: 60 } });
-
-        if (!response.ok) {
-            console.log('Error al obtener publicidades');
-        }
+        if (!response.ok) throw new Error('Failed to fetch ads');
         const { data } = await response.json();
-
-        console.log(data)
         return data;
     } catch (error) {
-        console.log(error)
+        console.error('Error fetching ads:', error);
+        return null;
     }
 }
 
@@ -51,48 +45,38 @@ interface SectionData {
     gridNews: SidebarItem[];
 }
 
-
 export default async function Page({ params }: { params: { slug: string[] } }) {
     const { slug } = params;
-    const [category, path] = slug;
+    const [category, path] = slug || [];
 
-    const decodeCategory = decodeURIComponent(category.replace(/\+/g, ' '));
+    if (!category) return notFound();
 
-    if (category && path) {
+    const decodedCategory = decodeURIComponent(category.replace(/\+/g, ' '));
+    let news, moreNews, ads, homeNews;
 
-        const [news, moreNews, ads] = await Promise.all([
-            getNewsByPath(path),
-            getFormatedCategoryNews(decodeCategory),
-            getAds(decodeCategory)
+    try {
+        [news, homeNews, moreNews, ads] = await Promise.all([
+            path ? getNewsByPath(path) : Promise.resolve(null),
+            getCover(),
+            getFormatedCategoryNews(decodedCategory),
+            getAds(decodedCategory)
         ]);
-
-        return (
-            <Container news={news} category={decodeCategory} moreNews={moreNews} ads={ads} />
-        );
+    } catch (error) {
+        console.error('Error fetching data:', error);
+        return notFound();
     }
 
-    if (category && !path) {
-
-        const sectionTitle = decodeCategory.charAt(0).toUpperCase() + decodeCategory.slice(1);
-
-        const [homeNews, ads, moreNews] = await Promise.all([
-            getCover(),
-            getAds(decodeCategory),
-            getFormatedCategoryNews(category)
-        ]);
-
-
+    if (path && news) {
+        return (
+            <Container news={news} category={decodedCategory} moreNews={moreNews} ads={ads} />
+        );
+    } else if (!path && homeNews) {
+        const sectionTitle = decodedCategory.charAt(0).toUpperCase() + decodedCategory.slice(1);
         const { data: { sections } } = homeNews;
-
-        const news: SectionData = sections[decodeCategory];
+        const news: SectionData = sections[decodedCategory];
 
         const allNews = [news.mainNews, ...news.gridNews];
-
-        const filteredNews = moreNews.filter((item: NewsType) => {
-            return !allNews.some((newsItem) => newsItem.id === item._id);
-        });
-
-        console.log(moreNews, decodeCategory)
+        const filteredNews = moreNews.filter((item: NewsType) => !allNews.some(newsItem => newsItem.id === item._id));
 
         return (
             <main>
@@ -101,78 +85,7 @@ export default async function Page({ params }: { params: { slug: string[] } }) {
                     <Navbar />
                     <QuotesContainer />
                 </div>
-                <div className="py-10">
-                    <div className="relative">
-                        <SectionTitle title={sectionTitle} />
-                        {
-                            allNews.length &&
-                            <div className="max-w-6xl 2xl:mx-auto grid grid-cols-12 gap-4 px-3 lg:mr-[240px]">
-                                <NoticiaPrincipalCategory
-                                    image={{ type: news.mainNews.media.type as 'image' | 'video', url: news.mainNews.media.url }}
-                                    pretitle={news.mainNews.pretitle}
-                                    title={news.mainNews.title}
-                                    category={news.mainNews.category}
-                                    path={news.mainNews.path!}
-                                    id={news.mainNews.id}
-                                />
-
-                                <div className="col-span-12 lg:col-span-4 min-h-full">
-                                    <div className="grid grid-cols-12 gap-4 h-full">
-                                        {
-                                            news.gridNews.map((item, index) => (
-                                                <NoticiasGridCategory
-                                                    key={item.id}
-                                                    image={{ type: item.media.type as 'image' | 'video', url: item.media.url }}
-                                                    pretitle={item.pretitle}
-                                                    title={item.title}
-                                                    category={item.category}
-                                                    path={item.path!}
-                                                    id={item.id}
-                                                />
-                                            ))
-                                        }
-                                    </div>
-                                </div>
-                                {/* PUBLICIDAD INFERIOR DESKTOP  */}
-                                <InferiorDesktop url={ads?.media?.desktop?.bottom?.url || ''} />
-                            </div>
-                        }
-                        <div className="max-w-6xl 2xl:mx-auto px-3 lg:mr-[240px]">
-                            {
-                                filteredNews.map((item: Partial<NewsType>) => (
-                                    <Link href={`/${decodeCategory}/${item.path}`} key={item._id} className="flex flex-col-reverse md:flex-row justify-between gap-2 md:gap-10 border-b py-4 hover:bg-gray-50 p-1">
-                                        <div className="min-w-fit text-xs md:text-sm text-muted-foreground font-light " >8 de mayo de 2024</div>
-                                        <div className="flex flex-col space-y-2 grow order-first md:order-none">
-                                            <h3 className="text-2xl font-semibold leading-none tracking-tight">{item.title}</h3>
-                                            <h4 className={`${fontMerriweather.className} text-md`}>{item.summary}</h4>
-                                            <h5 className="text-muted-foreground text-sm">Por <b className="text-foreground capitalize">{item.author}</b></h5>
-                                        </div>
-                                        {
-                                            item.media?.portada.type !== 'video' ? (
-                                                <Image
-                                                    src={item.media?.portada.url ?? ''}
-                                                    alt={item.title ?? ''}
-                                                    width={400}
-                                                    height={300}
-                                                    className=" object-cover aspect-video rounded md:w-72 "
-                                                    placeholder="blur"
-                                                    blurDataURL={blurImage}
-                                                    priority
-                                                />
-                                            ) : (
-                                                <video width="400" height="300" controls={true} autoPlay loop className="w-full object-cover aspect-video rounded md:w-72 ">
-                                                    <source src={item.media?.portada.url} type="video/mp4" />
-                                                    Tu navegador no soporta la etiqueta de video.
-                                                </video>
-                                            )
-                                        }
-                                    </Link>
-                                ))
-                            }
-                        </div>
-
-                    </div>
-                </div>
+                <PortadaPrincipal sectionTitle={sectionTitle} allNews={allNews} news={news} ads={ads} filteredNews={filteredNews} />
             </main>
         );
     }
---
+++ src/components/noticias-por-categoria/PortadaPrincipal.tsx
@@ -0,0 +1,64 @@
+import React from 'react'
+import SectionTitle from '../home/SectionTitle'
+import NoticiaPrincipalCategory from '../home/categories/NoticiaPrincipal.category'
+import NoticiasGridCategory from '../home/categories/NoticiasGrid.category'
+import InferiorDesktop from '../home/publicidades/Inferior.Desktop'
+import NewsCardByCategory from './NewsCardByCategory'
+import { Ads, MainNews, SidebarItem } from '@/types/news.types'
+
+interface Props {
+    sectionTitle: string;
+    allNews: MainNews[];
+    news: {
+        mainNews: MainNews;
+        gridNews: SidebarItem[];
+    };
+    ads: any;
+    filteredNews: MainNews[];
+}
+
+export default function PortadaPrincipal({ sectionTitle, allNews, news, ads, filteredNews }: Props) {
+
+
+    return (
+        <div className="py-10">
+            <div className="relative">
+                <SectionTitle title={sectionTitle} />
+                {
+                    allNews.length &&
+                    <div className="max-w-6xl 2xl:mx-auto grid grid-cols-12 gap-4 px-3 lg:mr-[240px]">
+                        <NoticiaPrincipalCategory
+                            image={{ type: news.mainNews.media.type as 'image' | 'video', url: news.mainNews.media.url }}
+                            pretitle={news.mainNews.pretitle}
+                            title={news.mainNews.title}
+                            category={news.mainNews.category}
+                            path={news.mainNews.path!}
+                            id={news.mainNews.id}
+                        />
+
+                        <div className="col-span-12 lg:col-span-4 min-h-full">
+                            <div className="grid grid-cols-12 gap-4 h-full">
+                                {
+                                    news.gridNews.map((item, index) => (
+                                        <NoticiasGridCategory
+                                            key={item.id}
+                                            image={{ type: item.media.type as 'image' | 'video', url: item.media.url }}
+                                            pretitle={item.pretitle}
+                                            title={item.title}
+                                            category={item.category}
+                                            path={item.path!}
+                                            id={item.id}
+                                        />
+                                    ))
+                                }
+                            </div>
+                        </div>
+                        {/* PUBLICIDAD INFERIOR DESKTOP  */}
+                        <InferiorDesktop url={ads?.media?.desktop?.bottom?.url || ''} />
+                    </div>
+                }
+                <NewsCardByCategory news={filteredNews} />
+            </div>
+        </div>
+    )
+}
