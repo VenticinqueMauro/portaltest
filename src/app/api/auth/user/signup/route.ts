6fb03869975337f430dc18e44a2deb6025208807
Venticinque Mauro <102001296+VenticinqueMauro@users.noreply.github.com>
2024-06-06 09:49:29 -0300

    Prueba de registro de usarios con email de verficacion

--- src/app/api/auth/user/signup/route.ts
+++ src/app/api/auth/user/signup/route.ts
@@ -1,10 +1,10 @@
-import { sendEmail } from "@/actions/emails/verifyEmail"; 
-import { VerifyEmailTemplate } from "@/components/EmailTemplates/Verify.email"; 
-import { connectDB } from "@/lib/mongodb"; 
-import { ClientUser } from "@/models/client.user"; 
-import bcrypt from "bcrypt"; 
-import crypto from 'crypto'; 
-import { NextRequest, NextResponse } from "next/server"; 
+import { sendEmail } from "@/actions/emails/verifyEmail";
+import { VerifyEmailTemplate } from "@/components/EmailTemplates/Verify.email";
+import { connectDB } from "@/lib/mongodb";
+import { ClientUser } from "@/models/client.user";
+import bcrypt from "bcrypt";
+import crypto from 'crypto';
+import { NextRequest, NextResponse } from "next/server";
 
 export async function POST(req: NextRequest) {
 
@@ -16,7 +16,7 @@ export async function POST(req: NextRequest) {
         const userFound = await ClientUser.findOne({ email }); // Busca si el usuario ya está registrado
 
         if (userFound) {
-            return NextResponse.json({ message: 'Este correo electrónico ya ha sido registrado' }, { status: 400 }) // Devuelve un mensaje indicando que el correo ya está registrado si se encuentra en la base de datos
+            return NextResponse.json({ message: 'Este correo electrónico ya ha sido registrado' }, { status: 400 })
         }
 
         const hashedPassword = await bcrypt.hash(password, 10); // Hashea la contraseña
@@ -34,13 +34,13 @@ export async function POST(req: NextRequest) {
         await user.updateOne({ emailVerificationToken: emailVerificationToken }); // Actualiza el token de verificación de correo electrónico del usuario en la base de datos
 
         await sendEmail({
-            from: 'Portal de Prueba <onboarding@resend.dev>',
+            from: 'Registro | TENDENCIA DE NOTICIAS <onboarding@resend.dev>',
             to: [email],
             subject: "Verificación de Correo Electrónico",
             react: VerifyEmailTemplate({ email, emailVerificationToken }) as React.ReactElement, // Envía el correo electrónico de verificación
         })
 
-        return NextResponse.json({ message: 'Registro exitoso' }, { status: 200 }) // Devuelve un mensaje de registro exitoso si todo fue exitoso
+        return NextResponse.json({ message: 'Revisa tu correo para verificar tu cuenta' }, { status: 200 }) // Devuelve un mensaje de registro exitoso si todo fue exitoso
 
     } catch (error) {
         if (error instanceof Error) {
