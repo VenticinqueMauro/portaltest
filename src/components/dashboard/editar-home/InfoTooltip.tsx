3e5d6e1b09c6cc93943edcc9ca73180e8b66c124
Venticinque Mauro <102001296+VenticinqueMauro@users.noreply.github.com>
2024-07-04 07:58:21 -0300

    ajustes en mensaj ede tooltip editar home

--- src/components/dashboard/editar-home/InfoTooltip.tsx
+++ src/components/dashboard/editar-home/InfoTooltip.tsx
@@ -13,12 +13,13 @@ export default function InfoTooltip() {
                 <TooltipTrigger>
                     <Info />
                 </TooltipTrigger>
-                <TooltipContent className='bg-foreground max-w-sm text-wrap text-sm'>
-                    <h3 className='font-bold underline'>IMPORTANTE</h3>
+                <TooltipContent className="bg-foreground max-w-sm text-wrap text-sm">
+                    <h3 className="font-bold underline mb-1">Recuerde:</h3>
                     <p>
-                        Asegúrese de que las noticias guardadas en la portada y en las secciones tienen contenido de texto y multimedia confirmado.
-                        Si modifica una noticia después de cargarla en la página principal o en las categorías, estos cambios no se reflejarán.
-                        Deberá volver a cargar la noticia en la sección correspondiente. Solo se pueden cargar noticias en estado <b className="text-green-600">Publicado</b>.
+                        • En caso de editar una noticia, deberá volver a cargarla en la sección correspondiente para reflejar sus cambios.
+                    </p>
+                    <p>
+                        • Solo se podrán cargar aquellas noticias en estado <b className="text-green-600">Publicado</b>.
                     </p>
                 </TooltipContent>
             </Tooltip>
